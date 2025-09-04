0c6c5b977e9f49de37063a631e13ec6935729aa0
s394588 <s394588@students.cdu.edu.au>
2025-09-04 11:49:32 +1000

    Question no 3 edit

---
+++ demo.py
@@ -0,0 +1,7 @@
+import turtle
+screen = turtle.Screen()
+screen.title("Straight Line Example")
+t = turtle.Turtle()
+t.hideturtle()  
+t.forward(200)
+screen.mainloop()
---
+++ import turtle.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+t.hideturtle()   
+t.speed(3)
+def inward_line(length):
+    segment = length / 3
+    t.forward(segment)
+    t.left(60)
+    t.forward(segment)
+    t.right(120)
+    t.forward(segment)
+    t.left(60)
+    t.forward(segment)
+inward_line(300)
+turtle.done()
