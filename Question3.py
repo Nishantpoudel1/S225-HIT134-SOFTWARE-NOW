59bc29f07597c1b13a8b454d18647b0bbcc98d8a
Isha07-prog <ghisingisha3@gmail.com>
2025-09-04 12:44:52 +1000

    indentation pointing inward

--- Question3.py
+++ Question3.py
@@ -1,4 +1,5 @@
 '''
+<<<<<<< HEAD
 This is the code for the straight line using turtle graphics.
 
 '''
@@ -10,3 +11,23 @@ t = turtle.Turtle()
 t.hideturtle()  
 t.forward(200)
 screen.mainloop()
+=======
+this function belongs to indentation pointing inward
+'''
+
+import turtle
+t = turtle.Turtle()
+t.hideturtle()   
+t.speed(3)
+def inward_line(length):
+    segment = length / 3
+    t.forward(segment)
+    t.right(60)
+    t.forward(segment)
+    t.left(120)
+    t.forward(segment)
+    t.right(60)
+    t.forward(segment)
+inward_line(300)
+turtle.done()
+>>>>>>> 0bf6751 (indentation pointing inward)
