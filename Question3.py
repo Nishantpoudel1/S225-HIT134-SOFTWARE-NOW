4277f7dc09747cf634c040f6e587ca23b4d55932
UkeshMahato <ukeshmht100@gmail.com>
2025-09-04 12:42:58 +1000

    addition of the straight line

---
+++ Question3.py
@@ -0,0 +1,12 @@
+'''
+This is the code for the straight line using turtle graphics.
+
+'''
+
+import turtle
+screen = turtle.Screen()
+screen.title("Straight Line Example")
+t = turtle.Turtle()
+t.hideturtle()  
+t.forward(200)
+screen.mainloop()
--- demo.py
+++
@@ -1,7 +0,0 @@
-import turtle
-screen = turtle.Screen()
-screen.title("Straight Line Example")
-t = turtle.Turtle()
-t.hideturtle()  
-t.forward(200)
-screen.mainloop()
